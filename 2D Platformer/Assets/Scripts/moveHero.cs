0ecdabdd02cd890a2066a1b6911ca40bc57a2f64
ColinHite <ColinHite@users.noreply.github.com>
2015-09-13 20:02:12 -0600

    In this version I used scripting techniques to make the "Hero" jump and move left and right.

---
+++ 2D Platformer/Assets/Scripts/moveHero.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveHero : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		moveSpeed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (moveSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime, moveSpeed * Input.GetAxis ("Vertical") * Time.deltaTime, 0f);
+	}
+
+}
+
