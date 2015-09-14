a9e6cbb781abf0e4554fc0603ff42979cd0c3520
Colin Hite <colin.hite@gmail.com>
2015-09-14 11:17:25 -0600

    Work in class

---
+++ 2D Platformer/Assets/Scripts/newmove.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newmove : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody rb;
+	/*public float speed     = 5.0f;
+	public float jumpSpeed = 3.0f;
+	public float maxJump   = 3;
+	public float velocity  = 10.0f;
+	public int jumpCount   = 3;
+	bool isGrounded = true;
+*/
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		rb.AddForce (transform.("Horizontal") * thrust);
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	/*{
+		var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+		{
+			GetComponent<Rigidbody> ().AddForce (new Vector3 (move * speed * Time.deltaTime));
+		}
+	}
+}*/
