8c797b7bc28b0c353e78959c8037aed95efef0ba
Colin Hite <ColinHite@users.noreply.github.com>
2015-09-16 00:16:54 -0600

    Added Jump script. (Code having issues with jump function(not jumping))

--- 2D Platformer/Assets/Scripts/newmove.cs
+++ 2D Platformer/Assets/Scripts/newmove.cs
@@ -5,19 +5,30 @@ public class newmove : MonoBehaviour {
 	public float speed     = 5.0f;
 	public float jumpSpeed = 3.0f;
 	public float maxJump   = 3;
-	public float gravity  = 10.0f;
-	public int jumpCount   = 3;
+	public int jumpCount   = 2;
+	public bool isGrounded = true;
+	public float jumpForce = 10.00f;
 
-	// Use this for initialization
-	void Start () 
-	{
-	}
 	// Update is called once per frame
 	void Update () 
 	{
 		var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
 		{
 			GetComponent<Rigidbody> ().position += move * speed * Time.deltaTime;
+		}
+	
+		if (Input.GetKeyDown (KeyCode.Space)&&maxJump<3) {
+
+		GetComponent<Rigidbody> ().velocity += Vector3.up * jumpSpeed;
+		jumpCount++;
+		}
+		if(jumpCount>maxJump||isGrounded==false)
+		{
+			jumpSpeed=0.0f;
+		}
+		if (jumpCount > maxJump && isGrounded == true) {
+			jumpSpeed = 0.0f;
+			jumpCount = 2;
 		}
 	}
 }
