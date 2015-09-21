4d5dca500cf7cc12d1560d5fb929ef0d9eb9a015
Colin Hite <ColinHite@users.noreply.github.com>
2015-09-20 20:59:29 -0600

    Used "translate.forward" to force character orientation upon button press. What I found is that if I don't lock the z axis and perform a "compromising jump" (one that orients the nose to point upward) a button press of either assigned keys will force orient the character upright and back in its proper place.

---
+++ 2D Platformer/Assets/Scripts/rotationScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			transform.forward = new Vector3(0f,0f,-1f);	
+		}
+		if(Input.GetKeyDown (KeyCode.D))
+		{
+			transform.forward = new Vector3(0f,0f,1f);	
+		}
+	}
+
+}
