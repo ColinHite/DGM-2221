70e2b7aff94f8022834ad5306dca8b49c5518d07
Colin Hite <ColinHite@users.noreply.github.com>
2015-09-28 13:40:49 -0600

    Well I don't like how it is set up on this email. However, I was able to get a safe zone built which will guide the player with textboxes. I also started the scripting for that as well. Lastly I fixed the dificulty level and made the level fit more to a "snap" so that the jumps are more trackable. Things that I need to add and fix are finishing the script for text. Add a text to the begining for simple controls. Change the nose turn to be on the horizontal so you can use the arrow keys. Lastly add check points and a new element that is small. Thats it if you are not me and reading this im super excited to finish my game! :)

---
+++ 2D Platformer/Assets/Scripts/textFade.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class textFade : MonoBehaviour {
+
+	public Text welcome;
+	public float fadeSpeed = 5f;
+	public bool entrance;
+	public GameObject houseOne;
+
+	void Awake ()
+
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
