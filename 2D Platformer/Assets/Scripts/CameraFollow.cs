c27710aadb6cadcb245aa1f0c2c974f4eeeb0465
Colin Hite <ColinHite@users.noreply.github.com>
2015-09-27 18:25:26 -0600

    Completed Stage one of the game!

--- 2D Platformer/Assets/Scripts/CameraFollow.cs
+++ 2D Platformer/Assets/Scripts/CameraFollow.cs
@@ -12,6 +12,6 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.position = hero.transform.position + new Vector3 (0,0,-10);
+		transform.position = hero.transform.position + new Vector3 (0,0,-100);
 	}
 }
