cbe58479ae680b3702f57146e1b4b94ca85a41b7
Colin Hite <ColinHite@users.noreply.github.com>
2015-10-01 20:37:29 -0600

    Completed the text / UI system.
    IN PROGRESS::
    - Creating moving blocks
    - Checkpoints

--- 2D Platformer/Assets/Scripts/textFade.cs
+++ 2D Platformer/Assets/Scripts/textFade.cs
@@ -10,18 +10,46 @@ public class textFade : MonoBehaviour {
 	public GameObject houseOne;
 
 	void Awake ()
-
 	{
-
+		welcome = houseOne.GetComponentInChildren<Text> ();
+		welcome.color = Color.clear;
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		ColorChange ();
 	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Player") 
+		{
+			entrance = true;
+		}
+	}
+
+	
+	void OnTriggerExit (Collider col)
+	{
+		if (col.gameObject.tag == "Player") 
+		{
+			entrance = false;
+		}
+	}
+
+	void ColorChange ()
+	{
+		if (entrance) 
+		{
+			welcome.color = Color.Lerp (welcome.color, Color.white, fadeSpeed * Time.deltaTime);
+		}
+
+		if (!entrance) 
+		{
+			welcome.color = Color.Lerp (welcome.color, Color.clear, fadeSpeed * Time.deltaTime);
+		}
+
+	}
+
 }
