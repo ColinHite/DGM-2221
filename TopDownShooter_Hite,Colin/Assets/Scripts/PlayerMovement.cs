ba54d887ce8a7d6d220ee806ebe2e552a47e2472
ColinHite <chit9353@gmail.com>
2015-10-12 13:55:32 -0600

    This contains script to make new objects. What i still need to do is create a script to move the objects toward where the player is facing. Preferably add a health system and things as well.

--- TopDownShooter_Hite,Colin/Assets/Scripts/PlayerMovement.cs
+++ TopDownShooter_Hite,Colin/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,30 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
+
 	public float speed = 5.0f;
+
+	//This is for shooting projectiles
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+	//This is to shoot the projectiles
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time * fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	//This moves the character left right up and down while moving its forward facing position
 	void FixedUpdate () {
 		var move = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
 		{
