b5114ff4445f78916d20eea4660f2d5da7b30388
Colin H <colin.hite@gmail.com>
2015-11-09 00:05:19 -0700

    This is a very scary push today. Trying something new because sourcetree wanted to be freaky. Otherwise, the game should be built and ready to go. The parallaxing is working and up to snuff.

---
+++ CaveGame_Hite/Assets/Scripts/TileParallax.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileParallax : MonoBehaviour {
+
+
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
+	void Start ()
+    {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.right * newPosition;
+    }
+}
