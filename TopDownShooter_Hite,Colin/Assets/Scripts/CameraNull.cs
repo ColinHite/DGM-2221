25ede450986ba6e2e89db799f712e906a4407af5
Colin Hite <colin.hite@gmail.com>
2015-12-07 10:59:02 -0700

    This should be a state where I have some of the scene set up and ready. Currently I am working on the score.

---
+++ TopDownShooter_Hite,Colin/Assets/Scripts/CameraNull.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraNull : MonoBehaviour {
+	
+	public static Vector3 targetPos;
+	public static float ViewX;
+	public static float ViewZ;
+	public GameObject Player;
+	
+	
+	// Finds object to follow
+	
+	/*void Start () 
+	{
+		Player = GameObject.FindGameObjectWithTag ("CameraPoint");
+	}
+	
+	// Allows for the movment of the camera
+	*/
+	void FixedUpdate () 
+	{
+		ViewX = gameObject.transform.position.x;
+		ViewZ = gameObject.transform.position.z;
+		targetPos = new Vector3 (Player.transform.position.x, 0, Player.transform.position.z);
+		
+		ViewX -= (ViewX - targetPos.x);
+		ViewZ -= (ViewZ - targetPos.z);
+		gameObject.transform.position = new Vector3(ViewX, Player.transform.position.y, ViewZ);
+	}
+}
