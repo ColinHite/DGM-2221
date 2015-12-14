3c6366553e35c3bdd0095af466a29f2729b0674e
Colin Hite <colin.hite@gmail.com>
2015-12-14 10:12:25 -0700

    Got a more fixed version to work with a win pop-up.

---
+++ TopDownShooter_Hite,Colin/Assets/Scripts/Win.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Win : MonoBehaviour {
+
+	public GameObject win;
+	public GameObject hero;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	void OnTriggerEnter(Collider col)
+	{
+
+		if (col.gameObject.tag == "Player") {
+			win.SetActive (true);
+			hero.SetActive (false);
+		}
+	}
+}
